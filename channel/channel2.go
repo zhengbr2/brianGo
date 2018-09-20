d5968734fa00569978571acf2e3caf93c2ddf758
brian.zheng <brian.zheng@oocl.com>
2018-09-20 09:01:03 +0800

    refresh3

--- channel/channel2.go
+++ channel/channel2.go
@@ -11,9 +11,11 @@ func hello(done chan bool) {
 	fmt.Println("hello go routine awake and going to write to done")
 	done <- true
 }
+
+
 func main() {
 	done := make(chan bool)
-	fmt.Println("Main going to call hello go goroutine")
+	fmt.Println("Main going to call hello go goroutine!")
 	go hello(done)
 	<-done
 	fmt.Println("Main received data")
