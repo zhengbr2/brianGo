373b8bdca9cf01ebcb4faef9c64a6d27b73e2b99
zhengbr2 <17935129@qq.com>
2018-09-23 22:51:25 +0800

    I/O reader and write

--- io/reader_writer.go
+++ io/reader_writer.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+		"bytes"
 	"os"
-	"bytes"
 )
 
 func main() {
 	//定义零值Buffer类型变量b
 	var b bytes.Buffer
 	//使用Write方法为写入字符串
-	b.Write([]byte("你好"))
+	b.Write([]byte("hello"))
 
 	//这个是把一个字符串拼接到Buffer里
 	fmt.Fprint(&b,",","http://www.flysnow.org")
 	fmt.Fprint(&b,"\n","another line from brian")
 	//把Buffer里的内容打印到终端控制台
 	b.WriteTo(os.Stdout)
+	//b.Reset()
+
+	b.Write([]byte("ILoveYou"))
+	var p=[]byte{'a','b','c','d','e'}
+	n,err:=b.Read(p[:])
+	fmt.Println(n,err,string(p[:n]))   // abcde-> ILove
 }
