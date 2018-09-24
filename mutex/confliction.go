993d641b15c0849edb2bb6b5c5f49321aa475171
zhengbr2 <17935129@qq.com>
2018-09-24 18:32:38 +0800

    ...

---
+++ mutex/confliction.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	count int32
+	wg    sync.WaitGroup
+)
+
+func main() {
+	wg.Add(2)
+	go incCount()
+	go incCount()
+	wg.Wait()
+	fmt.Println(count)
+}
+
+func incCount() {
+	defer wg.Done()
+	for i := 0; i < 2; i++ {
+		value := count
+		runtime.Gosched()
+		value++
+		count = value
+	}
+}
