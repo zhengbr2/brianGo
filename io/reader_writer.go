5ba6d05b48383307320bc4c5d54d0416c7d2b1d9
brian.zheng <brian.zheng@oocl.com>
2018-09-25 13:43:07 +0800

    IO

--- io/reader_writer.go
+++ io/reader_writer.go
@@ -3,26 +3,40 @@ package main
 import (
 	"fmt"
 		"bytes"
+		"log"
 	"os"
-	)
+)
 
 func main() {
 	//定义零值Buffer类型变量b
 	var b bytes.Buffer
 	//使用Write方法为写入字符串
-	b.Write([]byte("hello"))
+	bn,be :=b.Write([]byte("hello"))
+	log.Println("number of bytes wrritten:",bn)
+	if be !=nil {
+		log.Fatal(be)
+	}
 
 	//这个是把一个字符串拼接到Buffer里
-	fmt.Fprint(&b,",","http://www.flysnow.org")
-	fmt.Fprint(&b,"\n","another line from brian")
+	fmt.Fprint(&b,",","http://www.flysnow.org\n")
+
 	//把Buffer里的内容打印到终端控制台
 	b.WriteTo(os.Stdout)
-	//b.Reset()
 
-	b.Write([]byte("ILoveYou"))
+
+	bn,be = b.Write([]byte("ILoveYou"))
+	log.Println("number of bytes wrritten:",bn)
+	log.Println("current length of b:",b.Len())
+	if be !=nil {
+		log.Fatal(be)
+	}
 	var p=[]byte{'a','b','c','d','e'}
 	n,err:=b.Read(p[:])
-	fmt.Println(n,err,string(p[:n]))   // abcde-> ILove
+	log.Println(n,err,string(p[:n]))   // abcde-> ILove
+	n,err=b.Read(p[:])
+	log.Println(n,err,string(p[:n]))   // abcde-> You
+	n,err=b.Read(p[:])
+	log.Println(n,err,string(p[:n]))   // _ 0 EOF
 
 
 }
