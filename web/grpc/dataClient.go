ca80dd9336c5bea3f5851f7cdb01e249fe15a70a
zhengbr2 <17935129@qq.com>
2018-09-26 22:20:28 +0800

    bentch mark grpc

--- web/grpc/dataClient.go
+++ web/grpc/dataClient.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"brianGo/web/grpc/example"
-	"golang.org/x/net/context"
+	"context"
 	"google.golang.org/grpc"
 	"log"
+	"sync"
 )
 
 // 定义请求地址
@@ -12,17 +13,32 @@ const (
 	ADDRESS string = "localhost:8080"
 )
 
+var wg sync.WaitGroup
+
 // main 方法实现对 gRPC 接口的请求
 func main() {
+
+	wg.Add(20)
+	for i := 0; i < 20; i++ {
+		go dial(i)
+	}
+	wg.Wait()
+
+}
+func dial(j int) {
 	conn, err := grpc.Dial(ADDRESS, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalln("Can't connect: " + ADDRESS)
 	}
-	defer conn.Close()
-	client := example.NewFormatDataClient(conn)
-	resp, err := client.DoFormat(context.Background(), &example.Data{Text: "hello,world!"})
-	if err != nil {
-		log.Fatalln("Do Format error:" + err.Error())
+	for i := 0; i < 1000; i++ {
+		client := example.NewFormatDataClient(conn)
+		resp, err := client.DoFormat(context.Background(), &example.Data{Text: "hello,world!"})
+		if err != nil {
+			log.Fatalln("Do Format error:" + err.Error())
+		}
+		log.Println(resp.Text, "in try#:", j*1000+i)
 	}
-	log.Println(resp.Text)
+	conn.Close()
+	wg.Done()
+
 }
