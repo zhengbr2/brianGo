67a49a23c21100987f496ffa6614cc1d40b2166e
brian.zheng <brian.zheng@oocl.com>
2018-09-28 16:43:33 +0800

    security session

--- web/http/httpsessions.go
+++ web/http/httpsessions.go
@@ -9,6 +9,8 @@ import (
 	_ "brianGo/web/http/session/memory"
 	"fmt"
 	"strings"
+	"crypto/md5"
+	"io"
 )
 
 var globalSessions *session.Manager
@@ -41,10 +43,35 @@ func count2(w http.ResponseWriter, r *http.Request) {
 	createtime := sess.Get("createtime")
 	if createtime == nil {
 		sess.Set("createtime", time.Now().Unix())
-	} else if (createtime.(int64) + 360) < (time.Now().Unix()) {
+	} else if (createtime.(int64) + 360) < (time.Now().Unix()) {    //expired time
 		globalSessions.SessionDestroy(w, r)
 		sess = globalSessions.SessionStart(w, r)
 	}
+	ct := sess.Get("countnum")
+	if ct == nil {
+		sess.Set("countnum", 1)
+	} else {
+		sess.Set("countnum", (ct.(int) + 1))
+	}
+
+	{
+		h := md5.New()
+		salt:="astaxie%^7&8888"
+		io.WriteString(h,salt+time.Now().String())
+		token:=fmt.Sprintf("%x",h.Sum(nil))
+		if r.Form["token"][0]!=token{
+			//提示登录
+		}
+		sess.Set("token",token)
+	}
+
+	t, _ := template.ParseFiles("count.gtpl")
+	w.Header().Set("Content-Type", "text/html")
+	t.Execute(w, sess.Get("countnum"))
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
 	ct := sess.Get("countnum")
 	if ct == nil {
 		sess.Set("countnum", 1)
@@ -71,7 +98,8 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/login", login) //设置访问的路由
-	http.HandleFunc("/count", count2)
+	http.HandleFunc("/count", count)
+	http.HandleFunc("/count2", count2)
 	http.HandleFunc("/", sayhelloName)
 
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
