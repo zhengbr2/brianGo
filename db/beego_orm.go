6bf8dabbf997c6b697ab068013cfc61f1b9652d8
brian.zheng <brian.zheng@oocl.com>
2018-09-28 16:13:01 +0800

    access mysql db

---
+++ db/beego_orm.go
@@ -0,0 +1,61 @@
+package main
+
+
+import (
+		"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+
+	"fmt"
+	"time"
+)
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	// 设置默认数据库
+	orm.RegisterDataBase("default", "mysql", "root:root@/test?charset=utf8", 30)
+	//注册定义的model
+
+	orm.RegisterModel(new(User))
+	// 创建table
+	orm.RunSyncdb("default", false, true)
+}
+
+
+
+type User struct {
+	Id     int `PK` //如果表的主键不是id，那么需要加上pk注释，显式的说这个字段是主键
+	Username    string
+	Departname  string
+	Created     time.Time
+}
+
+
+
+
+func main() {
+	o := orm.NewOrm()
+	var user User
+	user.Username = "brian"
+	user.Departname="Dev"
+	user.Created=time.Now()
+
+
+	id, err := o.Insert(&user)
+	if err == nil {
+		fmt.Println(id)
+	}
+
+	// 更新表
+	user.Username = "ivyfan"
+	num, err := o.Update(&user)
+	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+
+	// 读取 one
+	u := User{Id: user.Id}
+	err = o.Read(&u)
+	fmt.Printf("ERR: %v\n", err)
+
+	// 删除表
+	//num, err = o.Delete(&u)
+	//fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+}
---
+++ db/mysql.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	//"time"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/test?charset=utf8")
+	checkErr(err)
+
+	//插入数据
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("brian", "development", "2006-04-09")
+	checkErr(err)
+
+	{
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("dindin", "kindergarten", "2012-03-07")
+	checkErr(err)
+	id, err := res.LastInsertId()
+	log.Println("last insert id: ",id)
+	}
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	log.Println("last insert id:",id)
+	//更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("mingkun", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	log.Println("affected number:",affect)
+
+	//查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	//删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	log.Println("affected number after delete:",affect)
+
+	db.Close()
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
