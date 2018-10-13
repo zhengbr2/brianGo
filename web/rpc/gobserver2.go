7816d7711b08167240e7ee1433b16546cbfa98aa
zhengbr2 <17935129@qq.com>
2018-10-13 22:51:40 +0800

    use rpc accept () only. no need to call servConn explicitly

--- web/rpc/gobserver2.go
+++ web/rpc/gobserver2.go
@@ -43,22 +43,22 @@ func (this *Arith) Divide(req ArithRequest, res *ArithResponse) error {
 }
 
 func main() {
+
 	rpc.Register(new(Arith)) // 注册rpc服务
-
-
 	lis, err := net.Listen("tcp", "127.0.0.1:8095")
 	if err != nil {
 		log.Fatalln("fatal error: ", err)
 	}
 
 	fmt.Fprintf(os.Stdout, "%s", "start connection")
+	rpc.Accept(lis)
 
-	for {
-		conn,err:=lis.Accept()
-		if err !=nil {
-			log.Fatal(err)
-		}
-		go rpc.ServeConn(conn)
-	}
+	//for {
+	//	conn,err:=lis.Accept()
+	//	if err !=nil {
+	//		log.Fatal(err)
+	//	}
+	//	go rpc.ServeConn(conn)
+	//}
 
 }
