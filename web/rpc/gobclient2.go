01a3cc32fc906db4feda63c44e60afcfa58a7279
brian.zheng <brian.zheng@oocl.com>
2018-10-15 14:52:02 +0800

    rename

--- web/rpc/gobclient2.go
+++ web/rpc/gobclient2.go
@@ -20,7 +20,7 @@ type ArithResponse struct {
 }
 
 func main() {
-	conn, err := rpc.Dial("tcp", "127.0.0.1:8095")
+	rpcClient, err := rpc.Dial("tcp", "127.0.0.1:8095")
 	if err != nil {
 		log.Fatalln("dailing error: ", err)
 	}
@@ -28,13 +28,13 @@ func main() {
 	req := ArithRequest{9, 2}
 	var res ArithResponse
 
-	err = conn.Call("Arith.Multiply", req, &res) // 乘法运算
+	err = rpcClient.Call("Arith.Multiply", req, &res) // 乘法运算
 	if err != nil {
 		log.Fatalln("arith error: ", err)
 	}
 	fmt.Printf("%d * %d = %d\n", req.A, req.B, res.Pro)
 
-	err = conn.Call("Arith.Divide", req, &res)
+	err = rpcClient.Call("Arith.Divide", req, &res)
 	if err != nil {
 		log.Fatalln("arith error: ", err)
 	}
