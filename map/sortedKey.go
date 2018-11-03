81751f1898006ca91d824b344a82b0d50d8dcfc7
zhengbr2 <17935129@qq.com>
2018-11-03 17:57:19 +0800

    range map return in random order

---
+++ map/sortedKey.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+	var m = map[string]int{
+		"unix":         0,
+		"python":       1,
+		"go":           2,
+		"javascript":   3,
+		"testing":      4,
+		"philosophy":   5,
+		"startups":     6,
+		"productivity": 7,
+		"hn":           8,
+		"reddit":       9,
+		"C++":          10,
+	}
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for indx, k := range keys {
+		fmt.Println("Key:", k, "Value:", m[k], " index:",indx)
+	}
+}
