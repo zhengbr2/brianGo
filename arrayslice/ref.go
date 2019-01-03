2061e1f1791a6c08b8944af08175aaba501e756a
brian.zheng <brian.zheng@oocl.com>
2019-01-03 17:16:29 +0800

    ...

--- arrayslice/ref.go
+++ arrayslice/ref.go
@@ -15,3 +15,17 @@ func modify(ages []int){
 	fmt.Printf("函数里接收到slice的内存地址是%p\n",ages)
 	ages[0]=1
 }
+
+/*
+func (v Value) Pointer() uintptr {
+	// TODO: deprecate
+	k := v.kind()
+	switch k {
+	//省略无关代码
+	case Slice:
+		return (*SliceHeader)(v.ptr).Data
+	}
+}
+很明显了，当是slice类型的时候，返回是slice这个结构体里，字段Data第一个元素的地址。
+https://www.flysnow.org/2018/02/24/golang-function-parameters-passed-by-value.html
+*/
