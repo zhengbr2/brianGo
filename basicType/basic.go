7e9aa13cb41765d2078f4793de247efca3bbbbda
brian.zheng <brian.zheng@oocl.com>
2019-01-03 17:29:12 +0800

    ...

--- basicType/basic.go
+++ basicType/basic.go
@@ -9,15 +9,15 @@ func main(){
 	var b bool = false
 	fmt.Println(b)
 	var i int32 = 0
-	fmt.Println(unsafe.Sizeof(b))
-	fmt.Println(unsafe.Sizeof(i))
+	fmt.Println(unsafe.Sizeof(b))  //1
+	fmt.Println(unsafe.Sizeof(i))  //4
 	var ch byte = 65
 
-	println( string(rune(ch)))
+	fmt.Println( string(rune(ch)))
 	var r rune = 'C'
-	println( r)
-	println( string(r))
-	fmt.Println(unsafe.Sizeof(ch))
+	fmt.Println( r)  //rune is int32
+	fmt.Println( string(r))
+	fmt.Println(unsafe.Sizeof(ch))   //unit8
 	fmt.Println(unsafe.Sizeof(r))
 }
 
