5a78ee4550893362aa981c02625fb8e6132abd76
zhengbr2 <17935129@qq.com>
2019-01-06 19:16:37 +0800

    ..

--- errorHandling/panic.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	slice := make([]string, 2, 4)
-	Example(slice, "hello", 10)
-}
-func Example(slice []string, str string, i int) {
-	panic("Want stack trace")
-}
--- errorHandling/panicrecover.go
+++ errorHandling/panicrecover.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
 
 func recoverName() {
 	if r := recover(); r != nil {
@@ -11,12 +14,13 @@ func recoverName() {
 func fullName(firstName *string, lastName *string) {
 	defer recoverName()
 	if firstName == nil {
-		panic("runtime error: first name cannot be nil")
+		panic(errors.New("runtime error: first name cannot be nil"))
 	}
 	if lastName == nil {
 		fmt.Println("will trigger panic here")
 		panic("runtime error: last name cannot be nil ")
 	}
+	panic(999)
 	fmt.Printf("%s %s\n", *firstName, *lastName)
 	fmt.Println("returned normally from fullName")
 }
@@ -24,14 +28,17 @@ func fullName(firstName *string, lastName *string) {
 func main() {
 	defer fmt.Println("deferred call in main")
 	firstName := "Elon"
+	lastName :="Mask"
 	done := make(chan interface{})
 	go func() {
-		fullName(&firstName, nil)
+		fullName(nil, &lastName)
 		done <- 123
 	}()
 
 	<-done
 
-	fullName(nil, nil)
+	fullName(&firstName,nil)
+	fullName(&firstName, &lastName)
+
 	fmt.Println("returned normally from main")
 }
