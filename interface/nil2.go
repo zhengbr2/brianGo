0a81aaedeb8098524eb00946857bd53f1a274eac
brian.zheng <brian.zheng@oocl.com>
2019-01-08 10:15:58 +0800

    ..

--- interface/nil2.go
+++ interface/nil2.go
@@ -11,24 +11,34 @@ func (e *SyntaxError) Error() string { return e.msg }
 
 func Decode() *SyntaxError {
 	var err *SyntaxError
-	if false {
-		err = &SyntaxError{}
-	}
+	err = nil
 	return err
 }
 
 func Decode2() error { // 错误，将可能导致上层调用者err!=nil的判断永远为true。
 	var err *SyntaxError // 预声明错误变量
-	if false {
-		err = &SyntaxError{}
-	}
+	err = nil
 	return err // 错误，err永远等于非nil，导致上层调用者err!=nil的判断始终为true
 }
 
+func Decode4() error {  // 终极版
+	var err error
+	if (false){
+		err= &SyntaxError{}
+	}
+	return err
+}
+
 func main() {
-	s := Decode()
-	log.Println(s == nil)
+	s1 := Decode()
+	log.Println("s1 == nil",s1 == nil)
 
 	s2 := Decode2()
 	log.Println("s2 == nil",s2 == nil)
+
+	var s3 error =  Decode()
+	log.Println("s3 == nil",s3 == nil)
+
+	var s4 error =  Decode4()
+	log.Println("s4 == nil",s4 == nil)
 }
