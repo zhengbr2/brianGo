489dac6fe902556a8bfeff3f1b36dcb8206e8470
brian.zheng <brian.zheng@oocl.com>
2019-01-08 12:36:25 +0800

    ...

--- interviewgo/clouser2.go
+++ interviewgo/clouser2.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
 	wg := sync.WaitGroup{}
 	wg.Add(50)
-	for i := 0; i < 40; i++ {
+	for i := 0; i < 40; i++ {   //会导致输出的i不可控
 		go func() {
 			fmt.Println("i: ", i)
 			wg.Done()
 		}()
-		//time.Sleep(1)
+
 	}
+	time.Sleep(100)
+	fmt.Println("-----------------------------------------")
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			fmt.Println("i: ", i)
+			fmt.Println("in clouser i: ", i)
 			wg.Done()
 		}(i)
 	}
--- interviewgo/defer.go
+++ interviewgo/defer.go
@@ -11,7 +11,7 @@ func calc(index string, a, b int) int {
 func main() {
 	a := 1
 	b := 2
-	defer calc("index 1:", a, b)
+	defer calc("index 1:", a, b)  // defer 获得参数后， 不会受后来修改的影响
 	a = 6
 	b = 7
 	defer calc("index 2:", a, b)
