ede19965ca48a994c19215ab7cc48c5524699042
brian.zheng <brian.zheng@oocl.com>
2019-01-09 16:02:43 +0800

    .

--- map/reference.go
+++ map/reference.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	)
+	"unsafe"
+)
 func main() {
 	persons:=map[string]int{"brian":19,"quan":20}
 	p2:=persons
 	p3:=persons
 	fmt.Printf("原始map的内存地址是：%p\n",&persons)
-	fmt.Printf("原始map的内存地址是：%p\n",&p2)
-	fmt.Printf("原始map的内存地址是：%p\n",&p3)
+	fmt.Printf("p2的内存地址是：%p\n",&p2)
+	fmt.Printf("p3的内存地址是：%p\n",&p3)
+	fmt.Printf("persons：%d\n",unsafe.Sizeof(persons))
+	fmt.Printf("&persons：%d\n",unsafe.Sizeof(&persons))
+	fmt.Printf("p2：%d\n",unsafe.Sizeof(p2))
+	fmt.Printf("p3：%d\n",unsafe.Sizeof(p3))
+
 	modify(persons)
 	fmt.Println("map值被修改了，新值为:",persons)
 }
