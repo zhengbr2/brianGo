73ce9b6026546e64c1cf9f22051541307a8c44b1
brian.zheng <brian.zheng@oocl.com>
2019-01-22 10:20:01 +0800

    rpcx

--- web/rpcx/rpcxClient.go
+++ web/rpcx/rpcxClient.go
@@ -9,55 +9,65 @@ import (
 )
 
 func main() {
+
 	log.SetFlags(log.Lmicroseconds)
 	addr := "localhost:8972"
-	args := raw.Args{5, 6}
-	reply := &raw.Reply{}
-
 	d := client.NewPeer2PeerDiscovery("tcp@"+addr, "")
 
-	xclient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
-	defer xclient.Close()
+	arithCall(d)
+
+	echoCall(d)
+
+	timeQuery(d)
+}
+
+func arithCall(d client.ServiceDiscovery) {
+
+	args := raw.Args{5, 6}
+	reply := &raw.Reply{}
+	arithClient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer arithClient.Close()
 	log.Printf("args:%v", args)
-	err := xclient.Call(context.Background(), "Mul", args, reply)
+	err := arithClient.Call(context.Background(), "Mul", args, reply)
 	if err != nil {
 		log.Println("encountered error:", err)
 	}
 	log.Println("result for Mul:", reply.C)
-
-	err = xclient.Call(context.Background(), "Add", args, reply)
+	err = arithClient.Call(context.Background(), "Add", args, reply)
 	if err != nil {
 		log.Println("encountered error:", err)
 	}
 	log.Println("result for Add:", reply.C)
+}
+
+func echoCall(d client.ServiceDiscovery) {
 
 	echoClient := client.NewXClient("Echo", client.Failtry, client.RandomSelect, d, client.DefaultOption)
 	defer echoClient.Close()
-
 	args2 := "brian"
 	log.Printf("args:%v", args2)
 	var reply2 string
-
 	err2 := echoClient.Call(context.Background(), "Echo", args2, &reply2)
 	if err2 != nil {
 		log.Println("encountered error:", err2)
 	}
 	log.Println("result from Echo:", reply2)
+}
 
-	{
+func timeQuery(d client.ServiceDiscovery) {
 
-		timeClient := client.NewXClient("TimeS", client.Failtry, client.RandomSelect, d, client.DefaultOption)
-		defer timeClient.Close()
+	timeClient := client.NewXClient("TimeS", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer timeClient.Close()
 
-		tm := time.Now().Add(time.Hour * 3)
-		log.Println(tm)
+	tm := time.Now().Add(time.Hour * 3)
+	log.Println(tm)
 
-		var ret time.Time
+	var ret time.Time
 
-		errT := timeClient.Call(context.Background(), "Time", tm, &ret)
-		if errT != nil {
-			log.Println("encountered error:", errT)
-		}
-		log.Println("result from Time:", ret)
+	errT := timeClient.Call(context.Background(), "Time", tm, &ret)
+	if errT != nil {
+		log.Println("encountered error:", errT)
 	}
+	log.Println("result from Time:", ret)
+
 }
--- web/rpcx/rpcxClientStress.go
+++ web/rpcx/rpcxClientStress.go
@@ -4,28 +4,46 @@ import (
 	"context"
 	"github.com/smallnest/rpcx/client"
 	"log"
-	"strconv"
+	"sync"
 	"time"
+)
+
+var (
+	CLIENT_NUM   = 4
+	SINGLE_COUNT = 100000
 )
 
 func main() {
 	log.SetFlags(log.Lmicroseconds)
-	addr := "localhost:8972"
 
+	addr := "localhost:8972"
 	d := client.NewPeer2PeerDiscovery("tcp@"+addr, "")
+	before := time.Now()
+	wg := sync.WaitGroup{}
+	wg.Add(CLIENT_NUM)
+
+	for i := 0; i < CLIENT_NUM; i++ {
+		go queryTime(d, &wg)
+	}
+
+	wg.Wait()
+	log.Println("consumed time %f:", time.Now().Sub(before).Seconds())
+	log.Println("average %f:", float64(CLIENT_NUM*SINGLE_COUNT)/time.Now().Sub(before).Seconds())
+}
+
+func queryTime(d client.ServiceDiscovery, wg *sync.WaitGroup) {
 
 	timeClient := client.NewXClient("TimeS", client.Failtry, client.RandomSelect, d, client.DefaultOption)
-	defer timeClient.Close()
-	for i := 0; i < 100000; i++ {
+
+	defer func() { timeClient.Close(); wg.Done() }()
+
+	for i := 0; i < SINGLE_COUNT; i++ {
 
 		tm := time.Now().Add(time.Hour * 3)
-		log.Println("pricessing request" + strconv.Itoa(i))
-
 		var ret time.Time
 		errT := timeClient.Call(context.Background(), "Time", tm, &ret)
 		if errT != nil {
 			log.Println("encountered error:", errT)
 		}
-		log.Println("result from Time:", ret)
 	}
 }
