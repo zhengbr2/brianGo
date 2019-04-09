b90ef6df73081c4e14a83c56db949eb2f6be97cb
brian.zheng <brian.zheng@oocl.com>
2019-04-09 17:49:24 +0800

    b.N

--- test/BenchmarkSelect_test.go
+++ test/BenchmarkSelect_test.go
@@ -16,6 +16,23 @@ func BenchmarkSelectShared(b *testing.B) {
 	})
 }
 
+
+func BenchmarkSelectShared2(b *testing.B) {
+
+	b.ResetTimer()
+	idleShared := make(chan struct{})
+	for i := 0; i < b.N; i++ {
+		ch := make(chan int, 1)
+
+			select {
+			case ch <- 1:
+			case <-ch:
+			case <-idleShared:
+			}
+
+	}
+}
+
 func BenchmarkSelectPrivate(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		ch := make(chan int, 1)
