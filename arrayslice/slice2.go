7e30f585250504c56d6f036fd9ef7c6a6d6ea5ce
brian.zheng <brian.zheng@oocl.com>
2019-06-14 14:11:04 +0800

    xxx

--- arrayslice/slice2.go
+++ arrayslice/slice2.go
@@ -21,34 +21,30 @@ func main() {
 	fmt.Println("pers:", pers, "has new length", len(pers), "and capacity", cap(pers))
 	fmt.Println("sizeof:", unsafe.Sizeof(pers))   //24
 
+	n0 := make ([] int, 5,5)
+	n0 = append(n0,1,2,3)  // 0 0 0 0 0 1 2 3
 	n1 := make ([] int, 0,5)
 	n1 = append(n1, 1,2)
 	n2 := n1
+	n22 := n2
+	fmt.Printf("size of n2, %d \n", unsafe.Sizeof(n2))
+	fmt.Printf(" n1 have difference address with n2, %p, %p, %p \n",&n1,&n2, &n22)
 	n2=append(n2, 3)
 	fmt.Println(n1)   // [1,2]
 	fmt.Println(n2)   // [1,2,3]
-	fmt.Printf("address of n1:%p\n",n1)
+	fmt.Printf("address of n1:%p\n",n1) // slice 的头部 8 bytes是一个地址
 	fmt.Printf("address of n2:%p\n",n2)
 
 	sh1:=(*reflect.SliceHeader)(unsafe.Pointer(&n1))
-	fmt.Printf("A1 Data:%d,Len:%d,Cap:%d\n",sh1.Data,sh1.Len,sh1.Cap)
+	fmt.Printf("n1 Data:%x,Len:%d,Cap:%d\n",sh1.Data,sh1.Len,sh1.Cap)
 
 	sh2 :=(*reflect.SliceHeader)(unsafe.Pointer(&n2))
-	fmt.Printf("A Data:%d,Len:%d,Cap:%d\n", sh2.Data, sh2.Len, sh2.Cap)
+	fmt.Printf("n2 Data:%x, Len:%d,Cap:%d\n", sh2.Data, sh2.Len, sh2.Cap)
 
 	n3:=[]int {1,2}
 	fmt.Println("n3",n3)
 	n3=append(n3, 3)
 	fmt.Println("n3",n3)
-
-	m1:= map[int]string {
-		1:"brian",
-		2:"quan",
-	}
-
-	m1[3]="Ken"
-	fmt.Println(m1)
-
 
 
 }
